0b5f892dc768f63af17d483fe7c19d4d6002fd2c
huanchan <qinhuanchan@hotmail.com>
2016-06-05 14:58:24 +0800

    ComboBox联动选择

---
+++ WindowsFormsApplication1/ComboBox.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class ComboBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(31, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(31, 272);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(31, 103);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(121, 124);
+            this.listBox1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // ComboBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 361);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "ComboBox";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ComboBox_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ WindowsFormsApplication1/ComboBox.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class ComboBox : Form
+    {
+        public ComboBox()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_Load(object sender, EventArgs e)
+        {
+            //双击窗体进入编辑
+            this.comboBox1.Items.Add("财务部");
+            this.comboBox1.Items.Add("产品部");
+            this.comboBox1.Items.Add("销售部");
+            this.comboBox1.Items.Add("生产部");
+            //在comboBox中输入内容,注意名称前的c是小写
+            this.comboBox1.SelectedIndex = 1;
+            //comboBox1，默认选择的是产品部
+
+            this.comboBox2.Items.Add("财务部");
+            this.comboBox2.Items.Add("产品部");
+            this.comboBox2.Items.Add("销售部");
+            this.comboBox2.Items.Add("生产部");
+            this.comboBox2.SelectedIndex = 1;
+            //comboBox2，默认选择的是产品部
+
+            this.listBox1.Items.Add("财务部");
+            this.listBox1.Items.Add("产品部");
+            this.listBox1.Items.Add("销售部");
+            this.listBox1.Items.Add("生产部");
+            this.listBox1.SelectedIndex = 1;
+            //listBox1,默认选择的是产品部
+
+            this.textBox1.Text = "产品部";
+            //textBox1，默认选择的是产品部
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //双击comboBox，在变化选择的时候，下面的comboBox2、textBox1、listBox1也跟着改变
+            string mess = comboBox1.SelectedItem.ToString();
+            //声明一个字符串mess，它的内容等于选中的comboBox1内容。
+            comboBox2.SelectedItem = mess;
+            //comboBox2 中选中的内容=mess
+            listBox1.SelectedItem = mess;
+            textBox1.Text = mess;
+            //textBox1中的文字内容等于mess
+        }
+
+
+    }
+}
