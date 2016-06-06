ed950944355029e4933641cc36bfd038cbe81500
huanchan <qinhuanchan@hotmail.com>
2016-06-06 16:26:30 +0800

    多窗体传值搞不定！不管了

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,37 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("颜色反馈");
+            comboBox1.Items.Add("颜馈");
+            comboBox1.SelectedIndex = 0;
+            textBox2.Text = "";
+            textBox1.Focus();
+            //设置Form1 窗体的初始化源代码
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" | textBox2.Text == "")
+            {
+                MessageBox.Show("姓名或邮件地址不能为空！", "信息提示");
+            }
+            else
+            {
+                this.Hide();
+                //button所在的Form1隐藏
+                Form2 childForm2 = new Form2(this.textBox1.Text, this.textBox2.Text, this.comboBox1.SelectedItem.ToString());
+                childForm2.Show();
+                //Form2为将要接收Form1参数的窗体
+                //效果：点击“发送”，先判断Form1中文本框是否有参数，然后生成新的Form2，用以接受Form1中的参数
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- WindowsFormsApplication1/Form2.cs
+++ WindowsFormsApplication1/Form2.cs
@@ -12,9 +12,39 @@ namespace WindowsFormsApplication1
 {
     public partial class Form2 : Form
     {
+        private string p1;
+        private string p2;
+        private string p3;
+        //对Form2定义，明确定义了相关的参数
+        //创建Form2的类
         public Form2()
         {
             InitializeComponent();
+            //在private 变量中存储值。
+        }
+
+        public Form2(string varp1, string varp2, string varp3)
+        {
+            // TODO: Complete member initialization
+            this.p1 = varp1;
+            this.p2 = varp2;
+            this.p3 = varp3;
+            listBox1.Items.Add("姓名" + this.p1);
+            listBox1.Items.Add("邮件" + this.p2);
+            listBox1.Items.Add("主题" + this.p3);
+            //Form2类的实例化：使用InitializeComponent() 存储值。
+            //类的实例化，必须先声明变量，然后赋值。
+
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("感谢您输入的反馈！");
+            this.Close();
         }
     }
 }
